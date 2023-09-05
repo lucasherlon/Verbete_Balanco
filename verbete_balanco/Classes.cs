e9180c9fa0c51871036f6eb937c97c8a76bab220
lucasherlon <lucasherlondsmc@gmail.com>
2023-09-05 16:47:18 -0300

    Adicionar arquivos de projeto.

---
+++ verbete_balanco/Classes.cs
@@ -0,0 +1,33 @@
+﻿
+namespace verbete_balanco
+{
+    public class Phonetic
+    {
+        public string Text { get; set; }
+        public string Audio { get; set; }
+    }
+
+    public class Definition
+    {
+        public string definition { get; set; }
+        public string Example { get; set; }
+        public List<string> Synonyms { get; set; }
+        public List<string> Antonyms { get; set; }
+    }
+
+    public class Meaning
+    {
+        public string PartOfSpeech { get; set; }
+        public List<Definition> Definitions { get; set; }
+    }
+
+    public class Word
+    {
+        public string word { get; set; }
+        public string Phonetic { get; set; }
+        public List<Phonetic> Phonetics { get; set; }
+        public string Origin { get; set; }
+        public List<Meaning> Meanings { get; set; }
+    }
+
+}
---
+++ verbete_balanco/Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using verbete_balanco;
+
+var client = new HttpClient();
+string word = "";
+
+while(true)
+{
+    Console.Write("Digite uma palavra para saber seu significado ou 'sair' para encerrar: ");
+
+    word = Console.ReadLine().ToLower().Trim();
+    if (word == "sair")
+    {
+        Console.WriteLine("Encerrando...");
+        break;
+    }
+
+    try
+    {
+        var response = await client.GetAsync($"https://api.dictionaryapi.dev/api/v2/entries/en/{word}");
+        var content = await response.Content.ReadAsStringAsync();
+
+        var result = JsonConvert.DeserializeObject<List<Word>>(content);
+
+        foreach (Meaning mean in result[0].Meanings)
+        {
+            Console.WriteLine($"--> {mean.Definitions[0].definition}\n");
+        }
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Palavra não encontrada.");
+    }
+
+} 
+
+
+
